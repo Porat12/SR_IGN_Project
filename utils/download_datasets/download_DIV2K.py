58b541e5d24572a1ececb7352bb4c99f7d90c047
porat12 <porat326@gmail.com>
2025-11-14 15:43:58 +0200

    try to download DIV2K

---
+++ utils/download_datasets/download_DIV2K.py
@@ -0,0 +1,19 @@
+from torchvision.datasets.utils import download_and_extract_archive
+import os
+
+save_root = "../../data/DIV2K"     # <--- cleaner folder structure
+os.makedirs(save_root, exist_ok=True)
+
+# Download Train HR
+download_and_extract_archive(
+    url="https://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip",
+    download_root=save_root,
+    extract_root=os.path.join(save_root, "DIV2K_train_HR")
+)
+
+# Download Valid HR
+download_and_extract_archive(
+    url="https://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip",
+    download_root=save_root,
+    extract_root=os.path.join(save_root, "DIV2K_valid_HR")
+)
