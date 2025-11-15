446e638e0bfacf4fc79355974cd483ae481e99f2
Porat Hai <porat.hai@campus.technion.ac.il>
2025-11-15 19:26:53 +0200

    try to download again DIV2K

--- utils/download_datasets/download_DIV2K.py
+++ utils/download_datasets/download_DIV2K.py
@@ -5,16 +5,20 @@ import os
 save_root = "/rg/shocher_prj/porat.hai/SR_IGN_Project/data"     # for HPC
 os.makedirs(save_root, exist_ok=True)
 
+print("________________ Downloading DIV2K train Dataset __________________")
 # Download Train HR
 download_and_extract_archive(
     url="https://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip",
     download_root=save_root,
     extract_root=os.path.join(save_root, "DIV2K_train_HR")
 )
-
+print("________________ Downloading DIV2K validation Dataset __________________")
 # Download Valid HR
 download_and_extract_archive(
     url="https://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip",
     download_root=save_root,
     extract_root=os.path.join(save_root, "DIV2K_valid_HR")
 )
+print("________________ Done __________________")
+print("________________ Done __________________")
+print("________________ Done __________________")
