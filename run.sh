6aab2288238bd6fb01caea224e9143422c511306
porat12 <porat326@gmail.com>
2025-11-27 14:51:05 +0200

    celeba integration

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
+python main.py --train --relative_path_to_config MNIST_configurations/MNIST_conv_relu_maxpool.yaml
+
+
+python main.py --train --relative_path_to_config MNIST_configurations/MNIST_conv_relu.yaml
+
+
 python main.py --train --relative_path_to_config DIV2K_configurations/DIV2K_conv_relu.yaml
 
 
-
-python main.py --relative_path_to_config DIV2K_configurations/DIV2K_conv_relu.yaml
-
-
-
-python main.py --train --relative_path_to_config MNIST_configurations/MNIST_conv_relu_maxpool.yaml
+python main.py --train --relative_path_to_config CelebA_configurations/CelebA_conv_relu.yaml
